31a36661251ee9c033003bb91eff4920cd9cb95b
DanieleSavoia <danielesavoia44@gmail.com>
2023-04-04 17:07:37 +0200

    commit1

---
+++ script.js
@@ -0,0 +1,18 @@
+const userName = parseInt(prompt("Come ti chiami?"))
+const eleSpan = document.getElementById("nome")
+eleSpan.innerHTML = userName;
+
+
+
+const surname = parseInt(prompt("Qual'è il tuo cognome?"))
+const eleSpan2 = document.getElementById("cognome")
+eleSpan2.innerHTML = surname;
+
+
+
+
+const color = parseInt(prompt("Qual'è il tuo colore preferito?"))
+const eleSpan3 = document.getElementById("colore")
+eleSpan3.innerHTML = color;
+
+
