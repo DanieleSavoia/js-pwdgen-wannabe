6632304339a3808d412584e632442ec4aa4c5214
DanieleSavoia <danielesavoia44@gmail.com>
2023-04-04 17:45:40 +0200

    commit2

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const userName = parseInt(prompt("Come ti chiami?"))
+const userName = prompt("Come ti chiami?")
 const eleSpan = document.getElementById("nome")
 eleSpan.innerHTML = userName;
 
 
 
-const surname = parseInt(prompt("Qual'è il tuo cognome?"))
+const surname = prompt("Qual'è il tuo cognome?")
 const eleSpan2 = document.getElementById("cognome")
 eleSpan2.innerHTML = surname;
 
 
 
 
-const color = parseInt(prompt("Qual'è il tuo colore preferito?"))
-const eleSpan3 = document.getElementById("colore")
-eleSpan3.innerHTML = color;
+const colore = prompt("Qual'è il tuo colore preferito?")
+const eleSpan3 = document.getElementById("color")
+eleSpan3.innerHTML = colore;
 
 
+const eleSpan4 = document.getElementById("final")
